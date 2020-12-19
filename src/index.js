7d124af7fd1bd08febeefba29fb4d98e6d7cea7a
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-19 05:53:58 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// HERE COME OUR PAGES /////////////////
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+////////////////////////////////////////
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </>
+  </Router>),
   document.getElementById('root')
 );
